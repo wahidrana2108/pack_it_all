3e3383aa975ed714b2f43f76777492c25cf3f3c4
Wahid Rana <wahidzamanrana@gmail.com>
2022-11-17 10:54:20 +0600

    shop fixed

--- includes/testProductCard.php
+++ includes/testProductCard.php
@@ -27,7 +27,7 @@
     </div>
     <div class="col">
         <div class="card h-100">
-            <img src="admin_area/product_images/product-1.jpg" class="card-img-top" alt="...">
+            <img src="admin_area/product_images/product-3.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bolder text-primary">T-Shirt for Women</h5>
                 <h6 class="card-text">৳1200</h6>
@@ -40,7 +40,7 @@
     </div>
     <div class="col">
         <div class="card h-100">
-            <img src="admin_area/product_images/product-2.jpg" class="card-img-top" alt="...">
+            <img src="admin_area/product_images/product-4.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bolder text-primary">T-Shirt for Women</h5>
                 <h6 class="card-text">৳1200</h6>
@@ -53,7 +53,7 @@
     </div>
     <div class="col">
         <div class="card h-100">
-            <img src="admin_area/product_images/product-1.jpg" class="card-img-top" alt="...">
+            <img src="admin_area/product_images/product-5.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bolder text-primary">T-Shirt for Women</h5>
                 <h6 class="card-text">৳1200</h6>
@@ -66,7 +66,7 @@
     </div>
     <div class="col">
         <div class="card h-100">
-            <img src="admin_area/product_images/product-2.jpg" class="card-img-top" alt="...">
+            <img src="admin_area/product_images/product-6.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bolder text-primary">T-Shirt for Women</h5>
                 <h6 class="card-text">৳1200</h6>
