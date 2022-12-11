c980047efc41a6ac8c56bb28580dea61c08561a8
Wahid Rana <wahidzamanrana@gmail.com>
2022-12-11 13:22:15 +0600

    customer order done

--- customer/confirm.php
+++ customer/confirm.php
@@ -3,22 +3,45 @@
     $active='My Account';
     include("includes/navbar.php");
 ?>
+
+<?php
+    if(!isset($_SESSION['customer_email'])){
+        echo "<script>window.open('checkout.php','_self')</script>";
+    }
+    else{
+        if(isset($_GET['order_id'])){
+            $order_id = $_GET['order_id'];
+        }
+    }
+?>
+
 <!-- navbar end -->
 <div class="container col-md-6">
-<h1 class="text-center">LOGIN</h1>
-    <form action="checkout.php" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-            <label>Email Address</label>
-            <input type="email" class="form-control" name="c_email" required>
+<h1 class="text-center">Confirm Order</h1>
+    <form action="checkout.php?update='<?php echo $order_id; ?>'" method="post" enctype="multipart/form-data">
+        <div class="form-group pt-2">
+            <label>Invoice No:</label>
+            <input type="number" class="form-control" name="invoice_no" required>
         </div>
-        <div class="form-group">
-            <label>Enter Password</label>
-            <input type="password" class="form-control" name="c_pass" required>
+        <div class="form-group pt-2">
+            <label>Amount Sent:</label>
+            <input type="text" class="form-control" name="amount_sent" required>
+        </div>
+        <div class="form-group pt-2">
+            <label>Select Payment Method:</label>
+            <select class="form-select" aria-label="Default select example" required>
+                <option selected disabled>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+        </div>
+        <div class="form-group pt-2">
+            <label>Transaction/Reference ID:</label>
+            <input type="number" class="form-control" name="c_address" required>
         </div>
         <div class="text-center mt-2">
-            <button type="submit" name="login" value="login" class="btn btn-primary"><i class="fa-solid fa-right-to-bracket"></i> Login</button>
+            <button type="submit" name="login" value="login" class="btn btn-success"><i class="fa-sharp fa-solid fa-money-bill"></i> Confirm Payment</button>
         </div>
-        <small class="fw-bolder"><i>Forgot Password? <a href="customer_register.php">Click here</a></i></small><br>
-        <small class="fw-bolder"><i>Don't have an account? <a href="customer_register.php">Click here</a></i></small>
     </form>
 </div>
--- customer/includes/myorder.php
+++ customer/includes/myorder.php
@@ -2,7 +2,7 @@
     <thead>
       <tr>
         <th scope="col">#Order No</th>
-        <th scope="col">Amount</th>
+        <th scope="col">Due Amount</th>
         <th scope="col">Invoice No</th>
         <th scope="col">Quantity</th>
         <th scope="col">Size</th>
@@ -12,15 +12,42 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td><a class="btn btn-primary" href="confirm.php"><small>Confirm Payment</small></a></td>
-      </tr>
+
+      <?php
+        $customer_session = $_SESSION['customer_email'];
+        $get_customer = "select * from customers where customer_email='$customer_session'";
+        $run_customer = mysqli_query($con,$get_customer);
+        $row_customer = mysqli_fetch_array($run_customer);
+        $customer_id = $row_customer['customer_id'];
+        $get_orders = "select * from customer_order where customer_id='$customer_id'";
+        $run_orders = mysqli_query($con,$get_orders);
+        $i = 0;
+        while($row_orders = mysqli_fetch_array($run_orders)){
+          $order_id = $row_orders['order_id'];
+          $due_amount = $row_orders['due_ammount'];
+          $invoice_no = $row_orders['invoice_no'];
+          $qty = $row_orders['qty'];
+          $size = $row_orders['size'];
+          $order_date = substr($row_orders['order_date'],0,11);
+          $order_status = $row_orders['order_status'];
+          $i++;
+          if($order_status == 'Pending'){
+            $order_status = 'Unpaid';
+          }
+          else{
+            $order_status = 'Paid';
+          }
+          echo "
+          <tr>
+            <th scope='row'>#$i</th>
+            <td>$due_amount</td>
+            <td>$invoice_no</td>
+            <td>$qty</td>
+            <td>$size</td>
+            <td>$order_date</td>
+            <td>$order_status</td>
+            <td><a class='btn btn-primary' href='confirm.php?order_id=$order_id'><small>Confirm Payment</small></a></td>
+          </tr>";
+         } ?>
     </tbody>
   </table>
